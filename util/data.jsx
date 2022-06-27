43f03e05b9af45c539548a06349c373490c54700
Silivanxay Phetsouvanh <silivanxay.phetsouvanh@stengg.com>
2022-06-27 18:11:03 +0700

    Added Initial landing page components

---
+++ util/data.jsx
@@ -0,0 +1,137 @@
+export const heroContent = {
+  header: 'e-commerce',
+  headline: "Let's start your online shop 24/7",
+  context: [
+    {
+      text: 'Skip all those pain from using Facebook as your online store - no more same theme for every shop, no more online payment. Here, we let you customize your shop to suit your brand and style and all the rest of your e-commerce system is powered by our headless Bamboo BI e-commerce API',
+    },
+  ],
+};
+
+export const posContent = {
+  header: 'feature',
+  headline: 'Point of Salse - POS',
+  context: [
+    {
+      icon: 'flex-ui-assets/icons/Sales Reporting _ Analytics@2x-8.png',
+      text: 'Sales Reportings & Analytyics',
+    },
+    {
+      icon: 'flex-ui-assets/icons/Customer Management@2x-8.png',
+      text: 'Customer Managment',
+    },
+    {
+      icon: 'flex-ui-assets/icons/Integrations@2x-8.png',
+      text: 'Interated Inventory Management',
+    },
+    {
+      icon: 'flex-ui-assets/icons/A User-Friendly Interface@2x-8.png',
+      text: 'User-Friendly Interface',
+    },
+    {
+      icon: 'flex-ui-assets/icons/Apps and Integrations  @2x-8.png',
+      text: 'App Integration',
+    },
+  ],
+};
+
+export const posImage = [
+  '../flex-ui-assets/images/pos/sale.jpg',
+  '../flex-ui-assets/images/pos/dashboard.jpg',
+];
+
+export const storeContent = {
+  header: 'feature',
+  headline: 'Online Store-Front',
+  context: [
+    {
+      icon: 'flex-ui-assets/icons/Mobile-friendly, responsive design icon@2x-8.png',
+      text: 'Mobile-friendly, responsive design',
+    },
+    {
+      icon: 'flex-ui-assets/icons/High-quality images and videos@2x-8.png',
+      text: 'High-quality Images and Videos',
+    },
+    {
+      icon: 'flex-ui-assets/icons/A User-Friendly Interface@2x-8.png',
+      text: 'User-friendly navigation, search, and product filtering',
+    },
+    {
+      icon: 'flex-ui-assets/icons/FAQ sectio@2x-8.png',
+      text: 'FAQ section',
+    },
+    {
+      icon: 'flex-ui-assets/icons/Enticing product descriptions@2x-8.png',
+      text: 'Enticing product descriptions',
+    },
+  ],
+};
+
+export const storeImage = [
+  '../flex-ui-assets/images/storefront/store-pc.jpg',
+  '../flex-ui-assets/images/storefront/store-mobile.jpg',
+];
+
+export const inventoryContent = {
+  header: 'feature',
+  headline: 'Inventory Management',
+  context: [
+    {
+      icon: 'flex-ui-assets/icons/Purchase Order and Stock Management@2x-8.png',
+      text: 'Purchase order and stock managment',
+    },
+    {
+      icon: 'flex-ui-assets/icons/Sales Management@2x-8.png',
+      text: 'Sale management',
+    },
+    {
+      icon: 'flex-ui-assets/icons/Integrated Inventory Management@2x-8.png',
+      text: 'POS-Storefront Integrations',
+    },
+    {
+      icon: 'flex-ui-assets/icons/Sales Reporting _ Analytics@2x-8.png',
+      text: 'Reporting and data analytics',
+    },
+    {
+      icon: 'flex-ui-assets/icons/Inventory Forecasting@2x-8.png',
+      text: 'Inventory forecasting',
+    },
+  ],
+};
+
+export const inventoryImage = [
+  '../flex-ui-assets/images/inventory/inventory.jpg',
+  '../flex-ui-assets/images/inventory/inventory1@2x-100.jpg',
+];
+
+export const dMarketingContent = {
+  header: 'feature',
+  headline: 'Digital Marketing',
+  context: [
+    {
+      icon: 'flex-ui-assets/icons/branding@2x-8.png',
+      text: 'Branding design',
+    },
+    {
+      icon: 'flex-ui-assets/icons/seo@2x-8.png',
+      text: 'High SEO landing page',
+    },
+    {
+      icon: 'flex-ui-assets/icons/soscial@2x-8.png',
+      text: 'Social marketing and advertising',
+    },
+    {
+      icon: 'flex-ui-assets/icons/ananly@2x-8.png',
+      text: 'Google analytics',
+    },
+    {
+      icon: 'flex-ui-assets/icons/video@2x-8.png',
+      text: 'Short animation video',
+    },
+  ],
+};
+
+export const dMarketingImage = [
+  '../flex-ui-assets/images/digitalMarketing/digital marketing1-100.jpg',
+  '../flex-ui-assets/images/digitalMarketing/market@2x-100.jpg',
+];
