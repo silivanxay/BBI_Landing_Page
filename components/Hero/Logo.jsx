5943d8ab1a7fcdb7c584c5849486b570031dce58
PernDev <Pern.dev@gmail.com>
2022-08-22 15:51:54 +0700

    IMG

--- components/Hero/Logo.jsx
+++ components/Hero/Logo.jsx
@@ -1,17 +1,18 @@
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
-import IMG from "../../public/flex-ui-assets/icons/bamboobi.png"
+import IMG from "../../public/flex-ui-assets/logos/bamboobi.webp"
 
 export default function Logo(props) {
   return (
     <Link href="/">
       <a className="block "  >
 
-        <Image
+        <Image 
           onClick={props.onClick}
           // src={logo.image}
           src={IMG}
+          // scrset={("IMG", "70px", "40px")}
           // width="70"
           // height='40'
           alt="img"
