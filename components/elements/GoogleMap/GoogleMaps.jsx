fc6c5039913d7298b938d8123fafe7e204c76a18
PernDev <Pern.dev@gmail.com>
2022-09-04 16:58:33 +0700

    Product

--- components/elements/GoogleMap/GoogleMaps.jsx
+++ components/elements/GoogleMap/GoogleMaps.jsx
@@ -1,8 +1,30 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 const GoogleMaps = () => {
+  const defaultProps = {
+    center: {
+      lat: 17.95865078776269,
+      lng: 102.6263755,
+    },
+    zoom: 17,
+  };
   return (
     <div>
+      <div className="w-full h-96">
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyD6AUPIR0eIiGldIIo0b06uqLxlZDyQh-I" }}
+          defaultCenter={defaultProps.center}
+          defaultZoom={defaultProps.zoom}
+        >
+          <AnyReactComponent
+            lat={17.958650787762704}
+            lng={102.62650424603244}
+            text="Bamboo BI"
+          />
+        </GoogleMapReact>
+      </div>
       {/* <div className="w-full h-96">
 
       </div>  */}
